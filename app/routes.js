0c9d72c0f4499e585400433ec05f2064cdd6d470
Philip Hotton <philip.hotton@engineering.digital.dwp.gov.uk>
2026-05-01 14:20:36 +0100

    Updates to prototype for user testing
    Updates to screens, functionality, and inputs for user testing.
    
    GitOrigin-RevId: 7c010fab19f474df5f38ebab6fad1acfb06b827c

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,32 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+router.get('/record_a_check/case_details', function(req, res){
+    req.session.data.hasVisitedPageCaseDetails = true;
+
+    res.render('/record_a_check/case_details');
+});
+
+router.get('/record_a_check/sample_details', function(req, res){
+    req.session.data.hasVisitedPageSampleDetails = true;
+
+    res.render('/record_a_check/sample_details');
+});
+
+router.get('/record_a_check/award_details', function(req, res){
+    req.session.data.hasVisitedPageAwardDetails = true;
+
+    res.render('/record_a_check/award_details');
+});
+
+router.get('/record_a_check/assurance_overview', function( req, res){
+    const hasVisitedPageCaseDetails = req.session.data.hasVisitedPageCaseDetails
+    const hasVisitedPageSampleDetails = req.session.data.hasVisitedPageSampleDetails
+    const hasVisitedPageAwardDetails = req.session.data.hasVisitedPageAwardDetails
+
+    res.render('/record_a_check/assurance_overview', {
+        hasVisitedPageCaseDetails: hasVisitedPageCaseDetails,
+        hasVisitedPageSampleDetails: hasVisitedPageSampleDetails,
+        hasVisitedPageAwardDetails: hasVisitedPageAwardDetails
+    });
+});
